2b3273dded682260d68230d9b28450a9f06b02f4
gaetan_almela <gaetan.almela@gmail.com>
2018-12-28 00:07:49 -0500

    Made 3D cards and added contact email

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     checkHover();
+    hoverCards();
 
     let bodyHeigth = $(window).height() - 80;
 
@@ -59,4 +60,37 @@ function checkHover() {
 function getScreenDim() {
     console.log('width: ', $(window).width());
     console.log('height: ', $(window).height());
+}
+
+var lastTimeStamp;
+
+function hoverCards() {
+    var x, y;
+    let cards = $('.card');
+    for(c in cards) cards[c].id = 'card-' + c
+
+    cards.mousemove(function(e) {
+
+        x = e.pageX - $(this).offset().left;
+        y = e.pageY - $(this).offset().top;
+        width = $(this).width();
+        height = $(this).height();
+
+        xDeg = map(x, 0, width, -12, 12);
+        yDeg = map(y, 0, height, 12, -12);
+
+        $(this).css({
+            'transform' : `perspective(1000px) rotateX(${yDeg}deg) rotateY(${xDeg}deg)`
+        });
+    });
+
+    cards.on("mouseleave", function() {
+        $(this).css({
+            'transform' : `perspective(1000px) rotateX(0deg) rotateY(0deg)`
+        });
+    });
+}
+
+function map(input, lowIn, highIn, lowOut, highOut) {
+    return (input - lowIn) * (highOut - lowOut) / (highIn - lowIn) + lowOut;
 }
