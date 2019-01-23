d56bcfaaa9dd7ee27bfb0d1274637288f6fd9e04
llGaetanll <gaetan.almela@gmail.com>
2019-01-23 17:18:58 -0500

    fixed media scaling

--- main.js
+++ main.js
@@ -6,54 +6,52 @@ $(document).ready(function () {
     let bodyHeigth = $(window).height() - 80;
 
     $('body').css({
-        'height' : bodyHeigth
-    })
-
-    overviewHeight();
+        'height': bodyHeigth
+    });
 });
 
 function checkHover() {
     $("#1").hover(function () {
         $(".logo").css({
-            'background-image' : 'url("./logos/MobiusML.png")',
-            'display' : 'block'
+            'background-image': 'url("./logos/MobiusML.png")',
+            'display': 'block'
         });
     }, function () {
         $(".logo").css({
-            'display' : 'none'
+            'display': 'none'
         });
     });
 
     $("#2").hover(function () {
         $(".logo").css({
-            'background-image' : 'url("./logos/memedb.png")',
-            'display' : 'block'
+            'background-image': 'url("./logos/memedb.png")',
+            'display': 'block'
         });
     }, function () {
         $(".logo").css({
-            'display' : 'none'
+            'display': 'none'
         });
     });
 
     $("#3").hover(function () {
         $(".logo").css({
-            'background-image' : 'url("./logos/ChromaTilt.png")',
-            'display' : 'block'
+            'background-image': 'url("./logos/ChromaTilt.png")',
+            'display': 'block'
         });
     }, function () {
         $(".logo").css({
-            'display' : 'none'
+            'display': 'none'
         });
     });
 
     $("#4").hover(function () {
         $(".logo").css({
-            'background-image' : 'url("./logos/PuzzleTimer.png")',
-            'display' : 'block'
+            'background-image': 'url("./logos/PuzzleTimer.png")',
+            'display': 'block'
         });
     }, function () {
         $(".logo").css({
-            'display' : 'none'
+            'display': 'none'
         });
     });
 }
@@ -68,9 +66,9 @@ var lastTimeStamp;
 function hoverCards() {
     var x, y;
     let cards = $('.card');
-    for(c in cards) cards[c].id = 'card-' + c
+    for (c in cards) cards[c].id = 'card-' + c
 
-    cards.mousemove(function(e) {
+    cards.mousemove(function (e) {
 
         x = e.pageX - $(this).offset().left;
         y = e.pageY - $(this).offset().top;
@@ -81,41 +79,17 @@ function hoverCards() {
         yDeg = map(y, 0, height, 12, -12);
 
         $(this).css({
-            'transform' : `perspective(1000px) rotateX(${yDeg}deg) rotateY(${xDeg}deg)`
+            'transform': `perspective(1000px) rotateX(${yDeg}deg) rotateY(${xDeg}deg)`
         });
     });
 
-    cards.on("mouseleave", function() {
+    cards.on("mouseleave", function () {
         $(this).css({
-            'transform' : `perspective(1000px) rotateX(0deg) rotateY(0deg)`
+            'transform': `perspective(1000px) rotateX(0deg) rotateY(0deg)`
         });
     });
 }
 
 function map(input, lowIn, highIn, lowOut, highOut) {
     return (input - lowIn) * (highOut - lowOut) / (highIn - lowIn) + lowOut;
-}
-
-function overviewHeight() {
-    let wrappers = $('.block-wrapper');
-    for(w in wrappers) wrappers[w].id = w;
-    let shouldScroll = [];
-    
-    for(let i = 0; i < wrappers.length; i++) {
-        let currentWrapper = '#' + wrappers[i].id;
-        let height = $(currentWrapper).height();
-        let totalHeight = 0;
-        $(currentWrapper).children().each(function () {
-            totalHeight += $(this).height();
-        });
-
-        if(totalHeight > height) {
-            shouldScroll.push(true);
-            $(currentWrapper).addClass('scrollable');
-        } else if(height >= totalHeight) {
-            shouldScroll.push(false);
-        }
-    }
-    console.log(shouldScroll);
-
 }
