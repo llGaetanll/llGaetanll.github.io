32ec52c0a792fbccb90b55740a34e72bb951798b
llGaetanll <gaetan.almela@gmail.com>
2019-04-14 22:28:03 -0400

    updated eportfolio

--- data.js
+++ data.js
@@ -1,9 +1,153 @@
-
 const overview = {
-    "About Me": {
-
-    },
-    "Projects": {
-
-    }
+    "About Me": [
+        `^[https://i.imgur.com/Ktuxnr9.jpg]`,
+        `*[Hey!] I see that you've found my resume and I'm overjoyed 
+            that you would decide to spend your precious time reading it. 
+            So out of respect, I'll promise I'll keep it short.`,
+        `*[My name is Gaetan Almela] and I'm a French software developer. 
+            I moved to America when I was 14 and I've been programming since then.`,
+        `My projects and awards are listed here on this page and you 
+            can *[click] on them to find out more on my GitHub[https://github.com/llGaetanll]`,
+        `If you have any questions about my projects, you can contact me directly at 
+            *[gaetan.almela@gmail.com] and I'll be more than happy to get back to you!`
+    ],
+    "Education": [{
+            "title": "Gulliver Schools",
+            "subtitle": "Miami, FL",
+            "desc": "AUG 2015 - MAY 2019",
+            "about": `My high school i what got me into programming. I learned HTML 5 at the end of my freshman year and have continued to learn more since then.`
+        },
+        {
+            "title": "British School of Brussels",
+            "subtitle": "Brussels, Belgium",
+            "desc": "2013 - 2014",
+            "about": `This is where I learned to solve a rubik's cube for the first time. It's also where I was introduced to the idea of programming.`
+        },
+        {
+            "title": "Mougins School",
+            "subtitle": "Mougins, France",
+            "desc": "2011 - 2013 / 2014 - 2015",
+            "about": `Where I learned english.`
+        },
+        {
+            "title": "Ecole des Maurettes",
+            "subtitle": "Villeneuve Loubet, France",
+            "desc": "2007 - 2011",
+            "about": `General troublemaker, professional doodler.`
+        }
+    ],
+    "Outreach": [{
+            "title": "Amazon France",
+            "subtitle": "Scraping sales websites",
+            "desc": "GRADE 12",
+            "about": `I built a web scraper that Amazon in France uses to scrape sales websites and facilitate their work.`
+        },
+        {
+            "title": "Minds DB",
+            "subtitle": "Machine Learning with UC Berkeley",
+            "desc": "GRADE 12",
+            "about": `I contributed to mindsdb, a machine learning library started by UC berkeley students. I added the entire section about adding the library to Google colab. You can find more about the project here[https://github.com/mindsdb/mindsdb]`
+        },
+        {
+            "title": "Breakthrough Miami",
+            "subtitle": "Helping children in developing areas",
+            "desc": "GRADE 10 - GRADE 12",
+            "about": `Breakthrough Miami is a club in which we teach middle schoolers STEM related topics such as computer science, engineering and biomedical.`
+        }
+    ],
+    "Projects": [{
+            "title": "Mobius ML",
+            "subtitle": "A tool for quadriplegics using Machine Learning",
+            "desc": "GRADE 12",
+            "links": [{
+                    "name": "GitHub",
+                    "url": "https://github.com/Mobility-Based-Infrastructure-Using-ML/MobiusML"
+                },
+                {
+                    "name": "YouTube",
+                    "url": "https://www.youtube.com/watch?v=zeTphRB3KK8"
+                }
+            ],
+            "about": [
+                `Using a 3 layer feedforward Neural network built in tensorflow.js, this tool uses facial recognition to allow quadriplegics to move around with minimal effort and very little costs.`,
+                `An app for iOS is in the works.`
+            ]
+        },
+        {
+            "title": "MemeDB",
+            "subtitle": "A Social Network Database",
+            "desc": "FEB 16 2017 - ONGOING",
+            "about": [
+                `A social Network/database optimised for memes made to categorise, classify and understand the internet’s humor. It comprises of an app a website and a database which are currently in developement.`,
+                `I plan to use this data for personal research with *[machine learning], however I also plan on releasing public datasets for anyone to use.`,
+                `Unfortunately, for security reasons, the project was recently made private. Please contact me at *[gaetan.almela@gmail.com] if you would want to know more about it`
+            ]
+        },
+        {
+            "title": "ChromaTilt",
+            "subtitle": "An App for people with colorblindness",
+            "desc": "GRADE 11",
+            "links": [{
+                    "name": "GitHub",
+                    "url": "https://github.com/llGaetanll/Chroma-Tilt"
+                },
+                {
+                    "name": "TV",
+                    "url": "https://www.youtube.com/watch?v=679i0RwXcFg"
+                },
+                {
+                    "name": "Play Store",
+                    "url": "https://play.google.com/store/apps/details?id=com.almela.gaetan.chromatilt"
+                }
+            ],
+            "about": [
+                `An app meant for people with colorblindness available on the Google Play Store. It shifts the colors of the camera on your screen so people can better distinguish between colors. This was my first application`,
+                `The app will be updated to support iOS by switching to React Native`
+            ]
+        },
+        {
+            "title": "PuzzleTimer",
+            "subtitle": "An App for Rubik’s Cube speedsolving",
+            "desc": "GRADE 11 - ONGOING",
+            "links": [{
+                    "name": "GitHub",
+                    "url": "https://github.com/llGaetanll/PuzzleTimer"
+                },
+                {
+                    "name": "Plat Store",
+                    "url": "https://play.google.com/store/apps/details?id=com.almela.gaetan.timer"
+                }
+            ],
+            "about": `An App built using React Native that synchronizes rubik’s cube times across both the website and the app.`
+        },
+        {
+            "title": "Magic!",
+            "subtitle": "An app to facilitate Mathematics",
+            "desc": "GRADE 12 - ONGOING",
+            "links": [{
+                "name": "GitHub",
+                "url": "https://github.com/llGaetanll/PuzzleTimer"
+            }],
+            "about": [
+                `Allows the user to handwrite mathematical equations or operations on their devices.`,
+                `Equations are interpreted using a Neural Net and values can be assigned to the variables manually.`,
+                `Manipulation of equations is extremly intuitive (i.e. dragging variables around) allowing for complex operations and function chains.`
+            ]
+        }
+    ],
+    "Skills": [
+        `*[Languages] Javascript, HTML, CSS, Java, C++, Python`,
+        `*[Frameworks] p5.js, JQuery, React Native, Node js`,
+        `Can work with *[Illustrator, Photoshop, After Effects and Premiere Pro]`,
+        `Experienced with *[full stack] developement`,
+        `Can solve a Rubik’s cube in under 12 seconds`
+    ],
+    "Awards": [
+        `*[Published] social scientist in the journal of Gulliver Principles of Social Science - Who controls the Web`,
+        `Winner of the 2017 *[Congressional App Challenge] for ChromaTilt`
+    ],
+    "Languages": [
+        `Fluent English both in reading and writing.`,
+        `Fluent in French as first language.`
+    ]
 }
---
+++ js/overview.js
@@ -0,0 +1,130 @@
+$(document).ready(function () {
+    // parseToHTML(`My projects and awards are listed here on this page and you can *[click]* on them to find out more on my GitHub[https://github.com/llGaetanll]`);
+    loadData(overview);
+});
+
+function loadData(content) {
+    let parsedHTML = '';
+
+    // iterates every section in the object
+    for (let section in content) {
+        let sectionData;
+
+        console.log('testing "' + section + '" of type', typeof content[section][0]);
+        if (typeof content[section][0] == 'string') {
+            // About Me would match here
+            let strings = '';
+            content[section].forEach(str => {
+                strings += parseToHTML(str);
+            });
+
+            sectionData = 
+            `<div class="section" id="${section.toLowerCase().replace(/ /g, "-")}">
+                <h2>${section.toUpperCase()}</h2>
+                <div class="block-wrapper">
+                    ${strings}
+                    <div class="end"></div>
+                </div>
+            </div>`;
+        } else {
+            // Projects would match here
+            let actions = '';
+
+            content[section].forEach(obj => {
+                let subtitleHTML = obj['subtitle'] ? `<span>— ${obj['subtitle']}</span>` : '';
+                let titleHTML = obj['title'] ? `<h3>${obj['title']} ${subtitleHTML}</h3>` : '';
+
+                let descHTML = obj['desc'] ? `<p class="desc">${obj['desc']}</p>` : '';
+
+                let linksHTML;
+
+                // add buttons
+                if (obj['links']) {
+                    let links = obj['links'];
+                    linksHTML = '';
+
+                    links.forEach(l => {
+                        linksHTML += `<a class="ref-button" href="${l.url}"><h4>${l.name}</h4></a>`;
+                    });
+                }
+
+                let aboutHTML;
+                if (obj['about']) {
+                    let about = obj['about'];
+                    aboutHTML = '';
+
+                    if (typeof about == 'string') {
+                        aboutHTML = parseToHTML(about);
+                    } else {
+                        // if it's an array, add multiple strings
+                        about.forEach(str => {
+                            aboutHTML += parseToHTML(str);
+                        });
+                    }
+                }
+
+                const action =
+                `<div class="action">
+                    <div class="action-title">
+                        ${titleHTML}
+                        ${descHTML}
+                    </div>
+                    ${aboutHTML}
+                </div>`;
+
+                actions += action;
+            });
+
+            sectionData = 
+            `<div class="section" id="${section.toLowerCase().replace(/ /g, "-")}">
+                <h2>${section.toUpperCase()}</h2>
+                <div class="block-wrapper">
+                    ${actions}
+                    <div class="end"></div>
+                </div>
+            </div>`
+        }
+
+        parsedHTML += sectionData;
+    }
+
+    // console.log(parsedHTML);
+    $('.main').append(parsedHTML);
+}
+
+/*
+ * @param carkDown {string} carkDown - formatted String
+ * @returns {string} formattedHTML
+ */
+
+// each string is a paragraph
+// each paragraph can contain images, bold text, urls, or all of them
+function parseToHTML(carkDown) {
+    const imgUrlRegex = /(?<=\^\[)[^\]]*/g;
+    const boldTextRegex = /(?<=\*\[)[^\]]*/g;
+    const urlLinkRegex = /(?<=\w*\[)[^\]]*/g;
+    const urlNameRegex = /\w*(?=\[)/g;
+
+    const imgRegex = /\^\[.*\]/g;
+    const boldRegex = /\*\[.*\]/g;
+    // const urlRegex;
+
+    const regexs = [/img\[.*\]/g, /\*\[.*\]/g, ]
+    
+    // images
+    let url = carkDown.match(imgUrlRegex);
+    if(url){
+        console.log(url);
+        return `<img src="${url}" class="cover-photo" />`;
+    }
+
+    // bold text
+    let boldTexts = carkDown.match()
+    while(boldTexts) {
+        console.log(boldTexts);
+        carkDown =
+            carkDown.substring()
+    }
+
+    return `<p>${carkDown}</p>`;
+}
